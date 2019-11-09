de0493847619c5d2c161f527d00ba53583df9240
Mohamed Fathy <codetwostart@gmail.com>
2019-11-09 08:24:29 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace expressionbody
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add(2, 3);
+            Student s1 = CreateStudent("Mohamed", 22);
+            Console.WriteLine(s1.Name);
+        }
+        //static void Add(int fn, int sn) => Console.WriteLine($"{fn} + {sn} = {(fn + sn)}");
+        static int Add(int fn, int sn) => fn + sn;
+        static Student CreateStudent(string name, int age) => new Student() { Name = name, Age = age };
+    }
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
